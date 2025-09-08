e359066cc417ceea36136cbab1a78a1623a16134
jaderjader.99@gmail.com <jaderjader.99@gmail.com>
2025-09-07 20:47:28 -0500

    map y css

---
+++ GoogleMapsCoreMVC/GoogleMapsCoreMVC/Controllers/MapController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OpenStreetMapMVC.Controllers
+{
+    public class MapController : Controller
+    {
+        // Mapa básico (puedes moverte libremente)
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Varios marcadores con icono personalizado
+        public IActionResult Marcadores()
+        {
+            return View();
+        }
+
+        // Marcadores con ventanas emergentes (popup)
+        public IActionResult MarcadoresConPopup()
+        {
+            return View();
+        }
+
+    }
+}
